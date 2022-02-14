a84ed93d7b54c061d8e0cb255e0362b815ded1b6
Jiasheng-Yang <89425167+Jiasheng-Yang@users.noreply.github.com>
2022-02-14 15:04:08 -0700

    Add files via upload

--- chatterjack.py
+++ chatterjack.py
@@ -1,14 +1,18 @@
 from segmentation import Segment
+from greet_mess import Greetings
+
 
 if __name__ == '__main__':
     grab = Segment()
+    greet = Greetings()
+    greet.start()
     questions = input()
-    print(grab.entities(questions))
-    print(grab.interrogative(questions))
     while True:
-        if (questions == "Exit"):
+        if greet.ending(questions):
+            greet.endingMess()
             break
         else:
+            greet.nextQuestion()
             questions = input()
-            print(grab.entities(questions))
-            print(grab.interrogative(questions))
+            if greet.keepAsking(questions):
+                questions = input()
